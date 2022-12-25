7fa4397419ddc105c957c711bf7076decaf92fe4
Angori4 <pavlichuk0810@gmail.com>
2022-12-26 01:16:36 +0200

    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,58 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
 console.log(galleryItems);
+
+const galleryColection = document.querySelector('.gallery');
+
+const markup = createGalleryItemsMarkup(galleryItems);
+
+function createGalleryItemsMarkup(galleryItems) {
+    return galleryItems.map(({ description, original, preview }) => {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+    />
+   </a>
+   </div>`
+    }).join('');
+  
+}
+galleryColection.insertAdjacentHTML("beforeend", markup);
+
+
+galleryColection.addEventListener('click', onClickItem);
+
+function onClickItem(event) {
+    event.preventDefault();
+    const isGalleryImage = event.target.classList.contains('gallery__image');
+    if (!isGalleryImage) {
+        return;
+    }
+
+    const dataSourceValue = event.target.dataset.source;
+    console.log(dataSourceValue)
+
+    galleryColection.addEventListener('keydown', onEsc);
+
+   function onEsc(event) {
+    if (event.code === "Escape"){
+        instance.close();
+        console.log(event)
+    }
+    }
+    
+    const options = {
+    closable: true,
+    onShow:  (instance) => { document.addEventListener('keydown', onEsc); },
+	onClose: (instance) => { document.removeEventListener('keydown', onEsc); },
+    }
+
+    
+    const instance = basicLightbox.create(`
+    <img src="${dataSourceValue}" width="800" height="600">`, options);
+
+    instance.show()   
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
 console.log(galleryItems);
+
+const galleryColection = document.querySelector('.gallery');
+
+const markup = createGalleryItemsMarkup(galleryItems);
+
+function createGalleryItemsMarkup(galleryItems) {
+    return galleryItems.map(({ description, original, preview }) => {
+    return `
+    <li class="item"><a class="gallery__item" href="${original}">
+    <img
+    class="gallery__image"
+    src="${preview}"
+    alt="${description}"
+    />
+   </a></li>`
+    }).join('');
+  
+}
+galleryColection.insertAdjacentHTML("beforeend", markup);
+
+var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionPosition:'bottom', captionDelay: 250 });
